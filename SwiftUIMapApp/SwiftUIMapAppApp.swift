22ab4ced9b97345e0f6c3308f43feab1f9866866
Tazo <tazogigitashvili@gmail.com>
2023-06-18 22:13:00 +0400

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Tazo Gigitashvili on 18.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
