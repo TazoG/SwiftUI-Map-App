f8589b03869fa3b1f61af626294be7c7496f6d93
Tazo <tazogigitashvili@gmail.com>
2023-06-19 19:40:33 +0400

    Created locationListView and added ui ux stuff, also vm

--- SwiftUIMapApp/Models/Location.swift
+++ SwiftUIMapApp/Models/Location.swift
@@ -8,7 +8,8 @@
 import Foundation
 import MapKit
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
+    
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
@@ -21,5 +22,9 @@ struct Location: Identifiable {
         //cityName = "Rome"
         //id = "ColosseumRome"
         name + cityName
+    }
+    
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
     }
 }
